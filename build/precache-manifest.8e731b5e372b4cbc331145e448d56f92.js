7bebb14faf10acdcf4a1d2b722d3941ff55712d4
Sai Kumar <saikumar.c@prodapt.com>
2019-02-24 08:02:37 +0000

    commit project dir

---
+++ build/precache-manifest.8e731b5e372b4cbc331145e448d56f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b019887bb03d30cd0498",
+    "url": "/static/css/main.7b54296f.chunk.css"
+  },
+  {
+    "revision": "b019887bb03d30cd0498",
+    "url": "/static/js/main.b019887b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2ab0e0efde74c73a564fbce565d62351",
+    "url": "/index.html"
+  }
+];
